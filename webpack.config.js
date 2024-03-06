7d4da3d9972ba14922e76e98ee4d486c1ea580c3
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2024-03-06 23:14:03 +0000

    initial webpack commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: none,
+  entry: './src/index.js',
+  devtool: "source-map",
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
