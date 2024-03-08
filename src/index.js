a75c52c8bbcc707cfa24efb7d1f49f808058f979
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2024-03-08 22:44:43 +0100

    Import Modules & call newTodo
    create function	newTodo;
    This function will be the key function to create newTodos in the application, it call createTodo, which handles all the creation steps;
    This ensures code encapsulation and minimalize risk for future bugs;

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import todo_all from "./array";
+import { todo_all, priority_high, priority_medium, priority_low } from "./array";
 import createTodo from "./createTodo";
-
-/* Query Selector 
-const addTodoBtn = document.getElementById("add-todo-btn");
-addTodoBtn.addEventListener("click", () => {   });
-*/
 
 function newTodo(a, b, c, d) {
     createTodo(a, b, c, d)
-}
+};
 
-newTodo("The Odin Project", "Finnish the Odin Project to learn code", "31.12.2024", "High");
-console.log(todo_all);
+newTodo("Low", "test", "test", "low")
+newTodo("Medium", "Test", "13.13", "medium")
+newTodo("The Odin Project", "Finish the Odin Project to learn code", "31.12.2024", "high");
+console.log(todo_all, priority_high, priority_medium, priority_low);
