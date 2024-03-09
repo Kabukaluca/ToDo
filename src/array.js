c22fdba48b83b2c637d4f71f76b1b9dbe0125f32
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2024-03-09 19:39:24 +0100

    Todo Arrays
    This file stores all arrays that will be used to structure different projects/todo's lateron;

---
+++ src/array.js
@@ -0,0 +1,21 @@
+const todo_all = [];
+    /* Not yet used
+    const todo_today = [];
+    const todo_scheduled = [];
+    const todo_important = [];
+    const todo_completed = [];
+    */
+
+
+// === Projects === //
+const project_webdev = [];
+
+
+// --- Priorities --- //
+const priority_high = [];
+const priority_medium = [];
+const priority_low = [];
+
+
+// export default todo_all;
+export { todo_all, priority_high, priority_medium, priority_low };
