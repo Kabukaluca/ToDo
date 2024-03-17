6eaf037354c4a641513a9ab507256d0b9532f9f9
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2024-03-17 17:03:53 +0100

    remove unused files

--- src/clearArrays.js
+++
@@ -1,9 +0,0 @@
-import { priority_high, priority_medium, priority_low } from "./array";
-
-const clearArrays = () => {
-    priority_high.splice(0, priority_high.length);
-    priority_medium.splice(0, priority_medium.length);
-    priority_low.splice(0, priority_low.length);
-};
-
-export default clearArrays;
