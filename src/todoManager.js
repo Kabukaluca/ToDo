bb8251cdc31d668a99a0c8368e66c9ea6cde77d3
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2024-03-19 08:50:29 +0100

    Add custom ID to todo
    Add custom ID to each todo, to toggleStatus, or remove a todo from the TodoList without issues;

--- src/todoManager.js
+++ src/todoManager.js
@@ -2,15 +2,21 @@
 class TodoManager {
     constructor() {
         this.todoList = [];
+        this.nextId = 0;
     }
 
     todoAdd(todo) {
+        todo.id = this.nextId++;
         this.todoList.push(todo);
     }
 
     todoRemove(todoId) {
         this.todoList = this.todoList.filter(todo => todo.id !== todoId);
-    }
+        this.todoList.forEach((todo, index) => {
+            todo.id = index;
+        });
+        this.next = this.todoList.length;
+    };
 
     todoToggleStatus(todoId) {
         this.todoList.forEach(todo => {
