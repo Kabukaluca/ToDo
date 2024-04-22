655704e3e3a213c4cf0d609c8c14d85ebc4adfa8
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2024-04-22 22:40:52 +0200

    remove unused / updated code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import createTodo from "./Logic/createTodo";
-import { getStructuredTodos } from "./Logic/filterArrays";
 import displayContent from "./DOM/domStructure";
 import "./style.css";
 import { eventListenersSidebar } from "./DOM/domManager";
@@ -9,12 +8,12 @@ import { eventListenersSidebar } from "./DOM/domManager";
 function newTodo(a, b, c, d, e) {
     createTodo(a, b, c, d, e)
 };
+
+
  // Create new Todos //
 newTodo("Low Todo", "This should create a low prio todo", "1.1.23", "low", "complete")
-newTodo("Medium Todo", "This should create a medium prio todo", "13.13.23", "medium", "incomplete")
+newTodo("Medium Todo", "This should create a medium prio todo", "13.13.23", "medium", "complete")
 newTodo("The Odin Project", "Finish the Odin Project to learn code", "31.12.2024", "high", "incomplete");
-
-
 
 
 // === Console Log & Display === //
@@ -23,4 +22,3 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 displayContent();
-console.log("Test get Structure", getStructuredTodos());
