50ab58c666d5341f14bd6a4ec31864b56ac6a66c
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2024-06-24 09:07:21 +0200

    Add ProjectInput to new Todo creation Form
    Allows user to add their todos to certain Projects;
    Comment statusInput due to lack of need, if a todo is created it most certainly is not completed yet;

--- src/DOM/formModals.js
+++ src/DOM/formModals.js
@@ -1,4 +1,5 @@
 import { FormRowInput, FormRowTextarea, FormRowSelect } from "./domClasses";
+import { initialProjects } from "../Logic/formInputHandler";
 
 const newTodoModal = document.createElement("div");
     newTodoModal.setAttribute("id", "todo-form-modal");
@@ -13,7 +14,8 @@ const todoFormNameInput = new FormRowInput("Todo:", "text", "Todo", "Todo", "tru
 const todoFormDescriptionInput = new FormRowTextarea("Description:", "Description", "About xy..", "errorId");
 const todoFormDueDateInput = new FormRowInput("Due:", "date", "Due", "01.10.2030", "true", "due-error");
 const todoFormPriorityInput = new FormRowSelect("Priority:", "Priority", "required", "How important is this task ?", [{value: "High"}, {value: "Medium"}, {value: "Low"}]);
-const todoFormStatusInput = new FormRowInput("Complete:", "checkbox", "Complete", "Complete",);
+// const todoFormStatusInput = new FormRowInput("Complete:", "checkbox", "Complete", "Complete",);
+const todoFormProjectInput = new FormRowSelect("Project", "todo-form-project-select", "false", "", initialProjects);
 
 const submitTodoForm = document.createElement("input");
     submitTodoForm.setAttribute("type", "button");
@@ -29,8 +31,10 @@ const submitTodoForm = document.createElement("input");
             newTodoForm.appendChild(todoFormDescriptionInput.createFormRow());
             newTodoForm.appendChild(todoFormDueDateInput.createFormRow());
             newTodoForm.appendChild(todoFormPriorityInput.createFormRow());
-            newTodoForm.appendChild(todoFormStatusInput.createFormRow());
+            newTodoForm.appendChild(todoFormProjectInput.createFormRow());
+            // newTodoForm.appendChild(todoFormStatusInput.createFormRow());
             newTodoForm.appendChild(submitTodoForm);
+        
         };
 
     const closeNewTodoForm = () => {
@@ -75,4 +79,4 @@ const submitProjectForm = document.createElement("input");
         };
     };
 
-export { displayNewTodoForm, closeNewTodoForm, displayNewProjectForm, closeNewProjectForm  };
+export { displayNewTodoForm, closeNewTodoForm, displayNewProjectForm, closeNewProjectForm, initialProjects };
