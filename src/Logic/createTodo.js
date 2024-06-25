612ec0e0f68a369e6add5027bd10b750665feab3
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2024-06-25 10:18:22 +0200

    Include Project value for todo creation

--- src/Logic/createTodo.js
+++ src/Logic/createTodo.js
@@ -1,12 +1,14 @@
 import todoManager from "./todoManager.js";
 
 class Todo {
-    constructor(title, description, dueDate, priority, status) {
+    constructor(title, description, dueDate, priority, project, status) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.project = project;
         this.status = this.getStatus(status);
+
     }
 
     getStatus(status) {
@@ -19,14 +21,15 @@ class Todo {
             Description: this.description,
             Due: this.dueDate,
             Importance: this.priority,
+            Project: this.project,
             status: this.status
         };
         return data;
     };
 };
 
-function createTodo(title, description, dueDate, priority, todoStatus) {
-    let newTodo = new Todo(title, description, dueDate, priority, todoStatus);
+function createTodo(title, description, dueDate, priority, project, todoStatus) {
+    let newTodo = new Todo(title, description, dueDate, priority, project, todoStatus);
     todoManager.todoAdd(newTodo);
 };
 
